a92496d7742bb9e402af1f301d0af8d9dcfe19e3
PiggybankStudios <PrincessDungeonSmasher@gmail.com>
2024-09-21 12:59:01 -0700

    Missed some files in the previous commit

--- build/build.bat
+++ build/build.bat
@@ -1,23 +1,39 @@
 @echo off
+
+:: TODO: Do we need this?
 setlocal enabledelayedexpansion
 
-set ProjectName=Minimal Orca Application
-set ProjectNameSafe=MinOrca
+python --version 2>NUL
+if %ERRORLEVEL% NEQ 0 (
+	echo WARNING: Python isn't installed on this computer. Defines cannot be extracted from build_config.h!
+	exit /B %ERRORLEVEL%
+)
+
+:: +--------------------------------------------------------------+
+:: |                      Paths and Options                       |
+:: +--------------------------------------------------------------+
+for /f %%i in ('orca sdk-path') do set OrcaSdkPath=%%i
 
 set SourceFolder=../src
 set DataFolder=../data
+set LibFolder=../lib
 set AppIconPath=%DataFolder%/icon.png
-set WasmModulePath=module.wasm
+set OutputWasmModulePath=module.wasm
 set MainSourcePath=%SourceFolder%/main.c
+set BuildConfigPath=%SourceFolder%/build_config.h
+set ExtractDefineScriptPath=%LibFolder%/include/gylib/ExtractDefine.py
 
-for /f %%i in ('orca sdk-path') do set OrcaSdkPath=%%i
+for /f "delims=" %%i in ('python %ExtractDefineScriptPath% %BuildConfigPath% PROJECT_NAME')      do set PROJECT_NAME=%%i
+for /f "delims=" %%i in ('python %ExtractDefineScriptPath% %BuildConfigPath% PROJECT_NAME_SAFE') do set PROJECT_NAME_SAFE=%%i
+for /f "delims=" %%i in ('python %ExtractDefineScriptPath% %BuildConfigPath% DEBUG_BUILD')       do set DEBUG_BUILD=%%i
 
+:: +--------------------------------------------------------------+
+:: |                  Compiler and Linker Flags                   |
+:: +--------------------------------------------------------------+
 :: Main Flags
 :: --target=wasm32 = Output to wasm32 binary
 :: -mbuild-memory = Something about bulk memory operations like memset being opt-in instructions in wasm, this is how we make those functions compile to single instructions
-:: -g = Generate debug symbols
-:: -O2 = Optimization level 2
-set CompilerFlags=--target=wasm32 -mbulk-memory -g -O2
+set CompilerFlags=--target=wasm32 -mbulk-memory
 :: Linker Flags
 :: -Wl,--no-entry = Allow no entry point in this compilation (Orca will act as the entry point, and it will use our wasm binary when we do orca bundle)
 :: -Wl,--export-dynamic = ?
@@ -31,11 +47,22 @@ set CompilerFlags=%CompilerFlags% --sysroot %OrcaSdkPath%/orca-libc -I%OrcaSdkPa
 :: -lorca_wasm = This is the precompiled binary that we compile with in order to get all the orca API functions exposed to us
 set LinkerFlags=-L %OrcaSdkPath%/bin -lorca_wasm
 
+if "%DEBUG_BUILD%"=="1" (
+	REM -g = Generate debug symbols
+	REM -O2 = Optimization level 2
+	set CompilerFlags=%CompilerFlags% -g -O0
+) else (
+	REM -O2 = Optimization level 2
+	set CompilerFlags=%CompilerFlags% -O2
+)
+
 :: +--------------------------------------------------------------+
 :: |                     Compile Wasm Module                      |
 :: +--------------------------------------------------------------+
 echo [Compiling WASM Module...]
-clang %CompilerFlags% %LinkerFlags% -o %WasmModulePath% %MainSourcePath%
+
+clang %CompilerFlags% %LinkerFlags% -o %OutputWasmModulePath% %MainSourcePath%
+
 IF %ERRORLEVEL% NEQ 0 (
 	echo [Compilation Failed!]
 	EXIT /B %ERRORLEVEL%
@@ -46,4 +73,8 @@ IF %ERRORLEVEL% NEQ 0 (
 :: +--------------------------------------------------------------+
 :: |                   Bundle Orca Application                    |
 :: +--------------------------------------------------------------+
-orca bundle --name %ProjectNameSafe% --icon %AppIconPath% --resource-dir %DataFolder% %WasmModulePath%
+echo [Bunding Orca App...]
+
+orca bundle --name %PROJECT_NAME_SAFE% --icon %AppIconPath% --resource-dir %DataFolder% %OutputWasmModulePath%
+
+echo [Finished Bundling!]
--- src/main.c
+++ src/main.c
@@ -8,13 +8,23 @@ Description:
 */
 
 #include <math.h>
-
 #include <orca.h>
+
+#include "build_config.h"
+
+oc_surface surface;
+oc_canvas_renderer renderer;
+oc_canvas_context canvasContext;
 
 ORCA_EXPORT void oc_on_init()
 {
 	oc_log_info("oc_on_init!");
-	//TODO: Implement me!
+	
+	oc_window_set_title(OC_STR8(PROJECT_NAME_STR));
+	
+    renderer = oc_canvas_renderer_create();
+    surface = oc_canvas_surface_create(renderer);
+    canvasContext = oc_canvas_context_create();
 }
 
 ORCA_EXPORT void oc_on_terminate()
@@ -25,6 +35,7 @@ ORCA_EXPORT void oc_on_terminate()
 
 ORCA_EXPORT void oc_on_resize(u32 width, u32 height)
 {
+	oc_log_info("oc_on_resize(%d, %d)!", width, height);
 	//TODO: Implement me!
 }
 
@@ -40,6 +51,18 @@ ORCA_EXPORT void oc_on_key_up(oc_scan_code scan, oc_key_code key)
 
 ORCA_EXPORT void oc_on_frame_refresh()
 {
-	//TODO: Implement me!
+	oc_arena_scope scratch = oc_scratch_begin();
+	
+	oc_canvas_context_select(canvasContext);
+    oc_set_color_rgba(0xCC/255.0f, 0x3B/255.0f, 0x95/255.0f, 1); //FFCC3B95
+    oc_clear();
+    
+    oc_set_color_rgba(0xF2/255.0f, 0x7C/255.0f, 0xB1/255.0f, 1); //FFF27CB1
+    oc_rounded_rectangle_fill(10, 10, 100, 200, 15);
+    
+    oc_canvas_render(renderer, canvasContext, surface);
+    oc_canvas_present(renderer, surface);
+    
+    oc_scratch_end(scratch);
 }
 
