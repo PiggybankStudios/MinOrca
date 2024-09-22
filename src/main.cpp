169efca57ac04694a457bfd0e867dde890b577f2
PiggybankStudios <PrincessDungeonSmasher@gmail.com>
2024-09-22 09:09:24 -0700

    Moved some of the stuff in orca_aliases.h into orca_addons.h to try and distinguish between things I am adding on to the Orca stdlib functionality and things that are purely aliasing existing types and functions. Also added a main.h which defines AppState_t where most of our application state information resides

--- src/main.cpp
+++ src/main.cpp
@@ -11,32 +11,40 @@ Description:
 #include <orca.h>
 
 #include "orca_aliases.h"
+#include "orca_addons.h"
+#include "main.h"
 
 #include "build_config.h"
 
-OC_Surface_t surface;
-OC_CanvasRenderer_t renderer;
-OC_CanvasContext_t canvasContext;
-v2 mousePos;
-v2 ScreenSize;
-v2i ScreenSizei;
-rec ScreenRec;
-OC_Image_t pigTexture;
+AppState_t* app = nullptr;
+v2 MousePos = Vec2_Zero_Const;
+v2 ScreenSize = Vec2_Zero_Const;
+v2i ScreenSizei = Vec2i_Zero_Const;
+rec ScreenRec = Rec_Zero_Const;
 
+// +==============================+
+// |          OC_OnInit           |
+// +==============================+
 EXPORT void OC_OnInit()
 {
 	OC_Log_I("OC_OnInit!");
 	
 	OC_WindowSetTitle(NewStr(PROJECT_NAME_STR));
+	app = (AppState_t*)malloc(sizeof(AppState_t));
+	OC_Assert(app != nullptr, "Failed to allocate AppState_t structure!!");
+	memset(app, 0x00, sizeof(AppState_t)); //TODO: Change to ClearPntr?
 	
-	renderer = OC_CanvasRendererCreate();
-	surface = OC_CanvasSurfaceCreate(renderer);
-	canvasContext = OC_CanvasContextCreate();
+	app->renderer = OC_CanvasRendererCreate();
+	app->surface = OC_CanvasSurfaceCreate(app->renderer);
+	app->canvasContext = OC_CanvasContextCreate();
 	
-	pigTexture = OC_ImageCreateFromPath(renderer, NewStr("pig_invalid.png"), false);
-	OC_Assert(!OC_ImageIsNil(pigTexture), "Failed to load pig_invalid.png!");
+	app->pigTexture = OC_ImageCreateFromPath(app->renderer, NewStr("Image/pig_invalid.png"), false);
+	OC_Assert(!OC_ImageIsNil(app->pigTexture), "Failed to load pig_invalid.png!");
 }
 
+// +==============================+
+// |         OC_OnResize          |
+// +==============================+
 EXPORT void OC_OnResize(u32 width, u32 height)
 {
 	// OC_Log_I("OC_OnResize(%d, %d)!", width, height);
@@ -45,50 +53,62 @@ EXPORT void OC_OnResize(u32 width, u32 height)
 	ScreenRec = NewRec(0, 0, (r32)width, (r32)height);
 }
 
+// +==============================+
+// |         OC_OnKeyDown         |
+// +==============================+
 EXPORT void OC_OnKeyDown(OC_ScanCode_t scan, OC_KeyCode_t key)
 {
 	//TODO: Implement me!
 }
 
+// +==============================+
+// |          OC_OnKeyUp          |
+// +==============================+
 EXPORT void OC_OnKeyUp(OC_ScanCode_t scan, OC_KeyCode_t key)
 {
 	//TODO: Implement me!
 }
 
+// +==============================+
+// |        OC_OnMouseMove        |
+// +==============================+
 EXPORT void OC_OnMouseMove(float x, float y, float dx, float dy)
 {
-	mousePos.x = x;
-	mousePos.y = y;
+	MousePos.x = x;
+	MousePos.y = y;
 }
 
+// +==============================+
+// |      OC_OnFrameRefresh       |
+// +==============================+
 EXPORT void OC_OnFrameRefresh()
 {
 	OC_ArenaScope_t scratch = OC_ScratchBegin();
 	
-	OC_CanvasContextSelect(canvasContext);
+	OC_CanvasContextSelect(app->canvasContext);
 	OC_SetColorRgba(NewColorfHex(0xFFCC3B95));
 	OC_Clear();
 	
 	OC_SetColorRgba(NewColorfHex(0xFF222222));
-	// OC_ImageDraw(pigTexture, ScreenRec); //TODO: Color doesn't matter for OC_ImageDraw?
-	OC_SetImage(pigTexture);
+	// OC_ImageDraw(app->pigTexture, ScreenRec); //TODO: Color doesn't matter for OC_ImageDraw?
+	OC_SetImage(app->pigTexture);
 	OC_RectangleFill(ScreenRec);
 	
 	OC_SetColorRgba(NewColorfHex(0xFFF27CB1));
-	OC_SetImage(pigTexture);
-	OC_RoundedRectangleFill(NewRec(mousePos.x, mousePos.y, 100, 200), 25);
+	OC_SetImage(app->pigTexture);
+	OC_RoundedRectangleFill(NewRec(MousePos.x, MousePos.y, 100, 200), 25);
 	OC_SetImage(OC_ImageNil());
 	
-	OC_CircleFill(mousePos.x - 50 + 25, mousePos.y - 50 + 25, 50);
+	OC_CircleFill(MousePos.x - 50 + 25, MousePos.y - 50 + 25, 50);
 	
 	OC_SetColorRgba(NewColorfHex(0xFFF83333));
 	OC_SetWidth(20);
 	OC_MoveTo(100, 100);
-	OC_CubicTo(100, 100, mousePos.x, mousePos.y, 200, 100);
+	OC_CubicTo(100, 100, MousePos.x, MousePos.y, 200, 100);
 	OC_Stroke();
 	
-	OC_CanvasRender(renderer, canvasContext, surface);
-	OC_CanvasPresent(renderer, surface);
+	OC_CanvasRender(app->renderer, app->canvasContext, app->surface);
+	OC_CanvasPresent(app->renderer, app->surface);
 	
 	OC_ScratchEnd(scratch);
 }
---
+++ src/main.h
@@ -0,0 +1,18 @@
+/*
+File:   main.h
+Author: Taylor Robbins
+Date:   09\22\2024
+*/
+
+#ifndef _MAIN_H
+#define _MAIN_H
+
+struct AppState_t
+{
+	OC_Surface_t surface;
+	OC_CanvasRenderer_t renderer;
+	OC_CanvasContext_t canvasContext;
+	OC_Image_t pigTexture;
+};
+
+#endif //  _MAIN_H
