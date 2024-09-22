f73028f5cc49823acf258f6fe6355b250a5d59d9
Taylor Robbins (Piggybank Studios) <PrincessDungeonSmasher@gmail.com>
2024-09-21 22:16:01 -0700

    Completed the majority of orca_aliases.h. There's a few TODO: items left (for things like oc_strlist) and some TODO: items for things that we have in GyLib that Orca doesn't have equivalents for (like mat4) but the majority of the API and types have been aliased and all the code in main.cpp is now using our well-known naming convention

--- src/main.cpp
+++ src/main.cpp
@@ -14,72 +14,72 @@ Description:
 
 #include "build_config.h"
 
-oc_surface surface;
-oc_canvas_renderer renderer;
-oc_canvas_context canvasContext;
+OC_Surface_t surface;
+OC_CanvasRenderer_t renderer;
+OC_CanvasContext_t canvasContext;
 v2 mousePos;
 
-ORCA_EXPORT void oc_on_init()
+ORCA_EXPORT void OC_OnInit()
 {
-	oc_log_info("oc_on_init!");
+	OC_Log_I("OC_OnInit!");
 	
-	oc_window_set_title(OC_STR8(PROJECT_NAME_STR));
+	OC_WindowSetTitle(NewStr(PROJECT_NAME_STR));
 	
-    renderer = oc_canvas_renderer_create();
-    surface = oc_canvas_surface_create(renderer);
-    canvasContext = oc_canvas_context_create();
+    renderer = OC_CanvasRendererCreate();
+    surface = OC_CanvasSurfaceCreate(renderer);
+    canvasContext = OC_CanvasContextCreate();
 }
 
-ORCA_EXPORT void oc_on_terminate()
+ORCA_EXPORT void OC_OnTerminate()
 {
-	oc_log_info("oc_on_terminate!");
+	OC_Log_I("OC_OnTerminate!");
 	//TODO: Implement me!
 }
 
-ORCA_EXPORT void oc_on_resize(u32 width, u32 height)
+ORCA_EXPORT void OC_OnResize(u32 width, u32 height)
 {
-	oc_log_info("oc_on_resize(%d, %d)!", width, height);
+	OC_Log_I("oc_on_resize(%d, %d)!", width, height);
 	//TODO: Implement me!
 }
 
-ORCA_EXPORT void oc_on_key_down(oc_scan_code scan, oc_key_code key)
-{
-	//TODO: Implement me!
-}
-
-ORCA_EXPORT void oc_on_key_up(oc_scan_code scan, oc_key_code key)
+ORCA_EXPORT void OC_OnKeyDown(oc_scan_code scan, oc_key_code key)
 {
 	//TODO: Implement me!
 }
 
-ORCA_EXPORT void oc_on_mouse_move(float x, float y, float dx, float dy)
+ORCA_EXPORT void OC_OnKeyUp(oc_scan_code scan, oc_key_code key)
+{
+	//TODO: Implement me!
+}
+
+ORCA_EXPORT void OC_OnMouseMove(float x, float y, float dx, float dy)
 {
     mousePos.x = x;
     mousePos.y = y;
 }
 
-ORCA_EXPORT void oc_on_frame_refresh()
+ORCA_EXPORT void OC_OnFrameRefresh()
 {
-	oc_arena_scope scratch = oc_scratch_begin();
+	OC_ArenaScope_t scratch = OC_ScratchBegin();
 	
-	oc_canvas_context_select(canvasContext);
-    oc_set_color_rgba(0xCC/255.0f, 0x3B/255.0f, 0x95/255.0f, 1); //FFCC3B95
-    oc_clear();
+	OC_CanvasContextSelect(canvasContext);
+    OC_SetColorRgba(0xCC/255.0f, 0x3B/255.0f, 0x95/255.0f, 1); //FFCC3B95
+    OC_Clear();
     
-    oc_set_color_rgba(0xF2/255.0f, 0x7C/255.0f, 0xB1/255.0f, 1); //FFF27CB1
-    oc_rounded_rectangle_fill(mousePos.x, mousePos.y, 100, 200, 15);
+    OC_SetColorRgba(0xF2/255.0f, 0x7C/255.0f, 0xB1/255.0f, 1); //FFF27CB1
+    OC_RoundedRectangleFill(mousePos.x, mousePos.y, 100, 200, 15);
     
-    oc_circle_fill(mousePos.x - 50 + 15, mousePos.y - 50 + 15, 50);
+    OC_CircleFill(mousePos.x - 50 + 15, mousePos.y - 50 + 15, 50);
     
-    oc_set_color_rgba(0xF8/255.0f, 0x33/255.0f, 0x33/255.0f, 1); //0xFFF83333
-    oc_set_width(20);
-    oc_move_to(100, 100);
-    oc_cubic_to(100, 100, mousePos.x, mousePos.y, 200, 100);
-    oc_stroke();
+    OC_SetColorRgba(0xF8/255.0f, 0x33/255.0f, 0x33/255.0f, 1); //0xFFF83333
+    OC_SetWidth(20);
+    OC_MoveTo(100, 100);
+    OC_CubicTo(100, 100, mousePos.x, mousePos.y, 200, 100);
+    OC_Stroke();
     
-    oc_canvas_render(renderer, canvasContext, surface);
-    oc_canvas_present(renderer, surface);
+    OC_CanvasRender(renderer, canvasContext, surface);
+    OC_CanvasPresent(renderer, surface);
     
-    oc_scratch_end(scratch);
+    OC_ScratchEnd(scratch);
 }
 
